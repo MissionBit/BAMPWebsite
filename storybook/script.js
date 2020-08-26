1bf8bdc0321f2dc214ad122eb9f129f304a4121e
John Cipriano <johnmikecip@gmail.com>
2020-08-26 16:29:04 -0700

    automatically generate headings; rename sbPage -> sbDesktop

--- storybook/script.js
+++ storybook/script.js
@@ -3,7 +3,7 @@
  */
 function newButton(color) {
   const div = document.createElement("div")
-  div.className = `sbPage--${color}Btn`
+  div.className = `sbDesktop--${color}Btn`
   return div
 }
 
@@ -18,13 +18,42 @@ function addButtons(bar) {
 /**
  * Makes the fake titlebars.
  */
-function makeTitleBar(page) {
+function insertTitlebar(page) {
   const bar = document.createElement("div")
-  bar.className = ("sbPage--titlebar")
+  bar.className = ("sbDesktop--titlebar")
   addButtons(bar)
   page.insertBefore(bar, page.firstChild)
 }
 
-const pages = document.getElementsByClassName("sbPage")
-for (const page of pages)
-  makeTitleBar(page)
+/** Adds a heading to the section, with the module name. */
+function insertHeading(section, text) {
+  const h1 = document.createElement("h1")
+  h1.className = "sbHeading"
+  h1.innerText = text
+  section.insertBefore(h1, section.firstChild)
+}
+
+/**
+ * Makes the storybook "page" preview (desktop). Does this by moving
+ * the contents of the <section> into a new container div.
+ *
+ * TODO: rename
+ */
+function addDesktopPage(section) {
+  const contents = section.innerHTML
+  const page = document.createElement("div")
+  const className = section.children[0].className
+
+  // build page
+  page.className = "sbDesktop"
+  page.innerHTML = contents
+  insertTitlebar(page)
+
+  section.innerHTML = ""
+  section.appendChild(page)
+  insertHeading(section, "." + className)
+}
+
+const sections = document.getElementsByClassName("demo")
+for (const section of sections)
+  addDesktopPage(section)
