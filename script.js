8cf85647dac7746f755c4822c0b47622cd12e051
Tara-Tiong <51966355+Tara-Tiong@users.noreply.github.com>
2020-10-23 18:12:24 -0700

    cleaned up javascript

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
+function showNavModal() {
+    const navModal = document.getElementById('navModal')
+    navModal.classList.add('navModal-is-shown')
+}
+
+function hideNavModal() {
+    const navModal = document.getElementById('navModal')
+    navModal.classList.remove('navModal-is-shown')
+}
+
 const hamburgerBtn = document.getElementById('hamburger')
 hamburgerBtn.addEventListener('click', showNavModal)
-
 const closeNavModalBtn = document.getElementById('closeNavModal')
-closeNavModalBtn.addEventListener('click', navModal)
-
-function showNavModal() {
-    const navModalBtn = document.getElementById('navModal')
-    navModalBtn.classList.add('navModal-is-shown')
-}
-
-function navModal() {
-    const navModalBtn = document.getElementById('navModal')
-    navModalBtn.classList.remove('navModal-is-shown')
-}
+closeNavModalBtn.addEventListener('click', hideNavModal)
