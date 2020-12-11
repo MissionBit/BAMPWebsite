63d6f77e8a5e504b018520f05aeda6df5bf919bf
Ernesto2004 <ersarajyan@s.sfusd.edu>
2020-12-10 18:07:47 -0800

    Instagram Feed Finishing Touches

--- scripts/instaFeed.js
+++ scripts/instaFeed.js
@@ -8,7 +8,7 @@ async function instaFeedRequest(account) {
             console.log(json)
             return json
         })
-
+    console.log(response)
     if (response.graphql) {
         let media = response.graphql.user.edge_owner_to_timeline_media.edges
         for (const value in media) {
@@ -23,49 +23,16 @@ async function instaFeedRequest(account) {
                 let videoElement = document.createElement("video")
                 let sourceElement = document.createElement("source")
                 videoElement.classList.add("instaPost")
-                videoElement.controls = true;
+                videoElement.controls = "Play";
                 sourceElement.src = mediaObj.video_url
                 videoElement.appendChild(sourceElement)
                 posts.appendChild(videoElement)
 
             } else if (mediaObj.__typename == "GraphSidecar") {
-                const sidecarImages = mediaObj.edge_sidecar_to_children.edges
-                let carousel = document.createElement("DIV")
-                let carouselItems = document.createElement("DIV")
-                let carouselSliderPrev = document.createElement("DIV")
-                let carouselSliderNext = document.createElement("DIV")
-                carousel.classList.add("carousel")
-                carouselSliderPrev.className = "carousel--slider carousel--prev"
-                carouselSliderNext.className = "carousel--slider carousel--next"
-                carouselSliderPrev.innerHTML = "&#10094;"
-                carouselSliderNext.innerHTML = "&#10095;"
-                carouselItems.classList.add("carousel--items")
-                carousel.appendChild(carouselItems)
-                carouselItems.appendChild(carouselSliderPrev)
-                carouselItems.appendChild(carouselSliderNext)
-                console.log("/////////////////////////////")
-
-                for (let item in sidecarImages) {
-                    item = sidecarImages[item].node
-                    if (item.__typename == "GraphVideo") {
-                        console.log(item.video_url)
-                        let videoElement = document.createElement("video")
-                        let sourceElement = document.createElement("source")
-                        videoElement.className = "instaPost carousel--item"
-                        videoElement.controls = true;
-                        sourceElement.src = item.video_url
-                        videoElement.appendChild(sourceElement)
-                        carouselItems.appendChild(videoElement)
-                    } else if (item.__typename == "GraphImage") {
-                        console.log(item.display_url)
-                        let imgElement = document.createElement("img")
-                        imgElement.className = "instaPost carousel--item"
-                        imgElement.src = item.display_url
-                        carouselItems.appendChild(imgElement)
-                    }
-                }
-                console.log("/////////////////////////////")
-                posts.appendChild(carousel)
+                let imgElement = document.createElement("img")
+                imgElement.className = "instaPost carousel--item"
+                imgElement.src = mediaObj.display_url
+                posts.appendChild(imgElement)
             }
         }
     } else {
