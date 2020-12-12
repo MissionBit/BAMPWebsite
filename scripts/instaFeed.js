7b9d061d129059cc04b1316e452126189b45c5fe
John Cipriano <johnmikecip@gmail.com>
2020-12-11 17:37:19 -0800

    refactor JS

--- scripts/instaFeed.js
+++ scripts/instaFeed.js
@@ -1,44 +1,46 @@
-async function instaFeedRequest(account) {
-    const posts = document.querySelector(".instaPosts")
-    posts.innerHTML = ""
+/**
+ * Renders Instagram feed JSON response in the given DOM target.
+ */
+function renderFeed(response, target) {
+    target.innerHTML = ""
+    let media = response.graphql.user.edge_owner_to_timeline_media.edges
+    for (const value in media) {
+        const mediaObj = media[value].node
+        if (mediaObj.__typename == "GraphImage") {
+            let imgElement = document.createElement("img")
+            imgElement.classList.add("instagramFeed--post")
+            imgElement.src = mediaObj.display_url
+            target.appendChild(imgElement)
 
-    const response = await fetch(`https://www.instagram.com/${account}/?__a=1`)
-        .then(x => x.json())
-        .then(json => {
-            console.log(json)
-            return json
-        })
-    console.log(response)
-    if (response.graphql) {
-        let media = response.graphql.user.edge_owner_to_timeline_media.edges
-        for (const value in media) {
-            let mediaObj = media[value].node
-            if (mediaObj.__typename == "GraphImage") {
-                let imgElement = document.createElement("img")
-                imgElement.classList.add("instaPost")
-                imgElement.src = mediaObj.display_url
-                posts.appendChild(imgElement)
+        } else if (mediaObj.__typename == "GraphVideo") {
+            let videoElement = document.createElement("video")
+            let sourceElement = document.createElement("source")
+            videoElement.classList.add("instagramFeed--post")
+            videoElement.controls = "Play";
+            sourceElement.src = mediaObj.video_url
+            videoElement.appendChild(sourceElement)
+            target.appendChild(videoElement)
 
-            } else if (mediaObj.__typename == "GraphVideo") {
-                let videoElement = document.createElement("video")
-                let sourceElement = document.createElement("source")
-                videoElement.classList.add("instaPost")
-                videoElement.controls = "Play";
-                sourceElement.src = mediaObj.video_url
-                videoElement.appendChild(sourceElement)
-                posts.appendChild(videoElement)
-
-            } else if (mediaObj.__typename == "GraphSidecar") {
-                let imgElement = document.createElement("img")
-                imgElement.className = "instaPost carousel--item"
-                imgElement.src = mediaObj.display_url
-                posts.appendChild(imgElement)
-            }
+        } else if (mediaObj.__typename == "GraphSidecar") {
+            let imgElement = document.createElement("img")
+            imgElement.className = "instagramFeed--post carousel--item"
+            imgElement.src = mediaObj.display_url
+            target.appendChild(imgElement)
         }
-    } else {
-        alert("No such profile was found.")
-        return
     }
 }
 
-instaFeedRequest("bayareamuralpro")
+/**
+ * Fetches IG account feed JSON and renders (if valid response) into the given target.
+ */
+async function fetchAndRenderFeed(accountName, target) {
+    const response =
+      await fetch(`https://www.instagram.com/${accountName}/?__a=1`).then(x => x.json())
+
+    if(!response || !response.graphql) {
+        console.log(`Error: could not load IG profile for {accountName}.`)
+        return
+    }
+
+    renderFeed(response, target)
+}
