4609a25a5d19e2f2b004ad9a86c66e7d0769912b
Tara-Tiong <51966355+Tara-Tiong@users.noreply.github.com>
2021-01-15 18:02:13 -0800

    added muralGallery.css and muralGallery.js

--- scripts/muralGallery.js
+++ scripts/muralGallery.js
@@ -1,1 +1,8 @@
-mu
+function showMuralGallery() {
+    console.log('happy')
+    const muralGallery = document.getElementById("muralGallery")
+    muralGallery.classList.add('muralGallery-is-shown')
+}
+
+const showMore = document.getElementById("muralGallerySlider")
+showMore.addEventListener('click', showMuralGallery)
