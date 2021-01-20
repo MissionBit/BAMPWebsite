e96f2e0243c14426c532e824c59772641beefb85
Tara-Tiong <51966355+Tara-Tiong@users.noreply.github.com>
2021-01-19 18:39:00 -0800

    added replace function

--- scripts/muralGallery.js
+++ scripts/muralGallery.js
@@ -1,8 +1,37 @@
-function showMuralGallery() {
-    console.log('happy')
+function showMuralGallery(event) {
     const muralGallery = document.getElementById('muralGallery')
-    muralGallery.classList.add('muralGallery-is-shown')
+    muralGallery.classList.remove('muralGallery-is-hidden')
+    event.target.innerHTML = "&#10095;"
+    showMore.addEventListener('click', hideMuralGallery, {"once": true})
+    let muralGalleryText = document.getElementById('muralGalleryText').innerHTML
+    let changeMuralGalleryText = muralGalleryText.replace("Show More", "Hide")
+    document.getElementById("muralGalleryText").innerHTML = changeMuralGalleryText
+}
+
+// function showMuralGalleryText (event) {
+//     const muralGalleryText = document.getElementById('muralGalleryText')
+//     event.target.innerHTML = "Hide"
+//     showMore.addEventListener('click', hideMuralGalleryText, {"once": true})
+// }
+
+// function hideMuralGalleryText () {
+//     const muralGalleryText = document.getElementById('muralGalleryText')
+//     event.target.innerHTML = "Show More"
+//     showMore.addEventListener('click', showMuralGalleryText, {"once": true})
+// }
+
+function hideMuralGallery(event) {
+    const muralGallery = document.getElementById('muralGallery')
+    muralGallery.classList.add('muralGallery-is-hidden')
+    event.target.innerHTML = "&#10094;"
+    showMore.addEventListener('click', showMuralGallery, {"once": true})
+    let muralGalleryText = document.getElementById('muralGalleryText').innerHTML
+    let changeMuralGalleryText = muralGalleryText.replace("Hide", "Show More")
+    document.getElementById("muralGalleryText").innerHTML = changeMuralGalleryText
 }
 
 const showMore = document.getElementById('muralGallerySlider')
-showMore.addEventListener('click', showMuralGallery)
+showMore.addEventListener('click', showMuralGallery, {"once": true})
+
+// const showMoreText = document.getElementsByClassName(".muralGallery--text")
+// muralGalleryText.addEventListener('click', showMuralGalleryText, {"once": true})
