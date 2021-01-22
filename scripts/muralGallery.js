a6ad0d5a233cf03197d29d1213ae60529250460c
John Cipriano <johnmikecip@gmail.com>
2021-01-21 16:50:39 -0800

    comment out hide/show text for now

--- scripts/muralGallery.js
+++ scripts/muralGallery.js
@@ -3,9 +3,9 @@ function showMuralGallery(event) {
     muralGallery.classList.remove('muralGallery-is-hidden')
     event.target.innerHTML = "&#10095;"
     showMore.addEventListener('click', hideMuralGallery, {"once": true})
-    let muralGalleryText = document.getElementById('muralGalleryText').innerHTML
-    let changeMuralGalleryText = muralGalleryText.replace("Show More", "Hide")
-    document.getElementById("muralGalleryText").innerHTML = changeMuralGalleryText
+    // let muralGalleryText = document.getElementById('muralGalleryText').innerHTML
+    // let changeMuralGalleryText = muralGalleryText.replace("Show More", "Hide")
+    // document.getElementById("muralGalleryText").innerHTML = changeMuralGalleryText
 }
 
 function hideMuralGallery(event) {
@@ -13,9 +13,9 @@ function hideMuralGallery(event) {
     muralGallery.classList.add('muralGallery-is-hidden')
     event.target.innerHTML = "&#10094;"
     showMore.addEventListener('click', showMuralGallery, {"once": true})
-    let muralGalleryText = document.getElementById('muralGalleryText').innerHTML
-    let changeMuralGalleryText = muralGalleryText.replace("Hide", "Show More")
-    document.getElementById("muralGalleryText").innerHTML = changeMuralGalleryText
+    // let muralGalleryText = document.getElementById('muralGalleryText').innerHTML
+    // let changeMuralGalleryText = muralGalleryText.replace("Hide", "Show More")
+    // document.getElementById("muralGalleryText").innerHTML = changeMuralGalleryText
 }
 
 const showMore = document.getElementById('muralGallerySlider')
