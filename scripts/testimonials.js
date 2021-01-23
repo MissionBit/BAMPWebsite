9933c63531afad7e82099523231c246581d015fe
John Cipriano <johnmikecip@gmail.com>
2021-01-22 22:27:33 -0800

    fix indentation level

--- scripts/testimonials.js
+++ scripts/testimonials.js
@@ -1,31 +1,31 @@
 function setUpCarousel(root) {
     const siema = new SiemaWithDots({
-      selector: root.querySelector('.carousel--items'),
-      duration: 200,
-      easing: 'ease-out',
-      perPage: 1,
-      startIndex: 0,
-      draggable: true,
-      multipleDrag: true,
-      threshold: 20,
-      loop: true,
-      onInit: function() {
-        this.addDots()
-        this.updateDots()
-      },
-      onChange: function() {
-        this.updateDots()
-      },
+        selector: root.querySelector('.carousel--items'),
+        duration: 200,
+        easing: 'ease-out',
+        perPage: 1,
+        startIndex: 0,
+        draggable: true,
+        multipleDrag: true,
+        threshold: 20,
+        loop: true,
+        onInit: function() {
+            this.addDots()
+            this.updateDots()
+        },
+        onChange: function() {
+            this.updateDots()
+        },
     })
 
     const prevBtn = root.querySelector('.carousel--prev')
     if (prevBtn) {
-      prevBtn.addEventListener('click', () => siema.prev())
+        prevBtn.addEventListener('click', () => siema.prev())
     }
 
     const nextBtn = root.querySelector('.carousel--next')
     if (nextBtn) {
-      nextBtn.addEventListener('click', () => siema.next())
+        nextBtn.addEventListener('click', () => siema.next())
     }
 }
 
