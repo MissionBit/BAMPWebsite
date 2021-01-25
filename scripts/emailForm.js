c35cd4499332d2ec93e2923489ea891a588224de
John Cipriano <johnmikecip@gmail.com>
2021-01-24 16:45:21 -0800

    redirect email form

--- scripts/emailForm.js
+++ scripts/emailForm.js
@@ -4,3 +4,19 @@ const emailInput = document.querySelector('.emailForm--inputBox')
 for (const link of getUpdatesLinks) {
     link.addEventListener('click', () => emailInput.focus())
 }
+
+function openGoogleForm(event) {
+    const input = event.target.closest('.emailForm').querySelector(".emailForm--inputBox")
+    const email = input.value
+    event.preventDefault()
+
+    window.location.href = (
+        'https://docs.google.com/forms/d/e/' +
+        '1FAIpQLSc8P8mhGKiwYt87ab5hUlhywRYdZW4fxUVscHmUtsTU37qEcQ' +
+        "/viewform?usp=pp_url&entry.359343040=" +
+        email
+    )
+}
+
+const submit = document.querySelector('.emailForm .btnLink')
+submit.addEventListener('click', openGoogleForm)
