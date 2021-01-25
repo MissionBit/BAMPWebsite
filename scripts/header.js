c94c06fa4c1e793ad5c8fa7b9544d493bb70b7ab
Tara-Tiong <51966355+Tara-Tiong@users.noreply.github.com>
2021-01-24 17:26:29 -0800

    added function --> hides navModal when btn clicked

--- scripts/header.js
+++ scripts/header.js
@@ -1,14 +1,18 @@
 function showNavModal() {
     const navModal = document.getElementById('navModal')
     navModal.classList.add('navModal-is-shown')
+    document.body.classList.add('l-noScroll')
 }
 
 function hideNavModal() {
     const navModal = document.getElementById('navModal')
     navModal.classList.remove('navModal-is-shown')
+    document.body.classList.remove('l-noScroll')
 }
 
 const hamburgerBtn = document.getElementById('hamburger')
 hamburgerBtn.addEventListener('click', showNavModal)
 const closeNavModalBtn = document.getElementById('closeNavModal')
 closeNavModalBtn.addEventListener('click', hideNavModal)
+const getUpdates = document.getElementById('getUpdates')
+getUpdates.addEventListener('click', hideNavModal)
