7e6c2608591ad2b806fbc5a8728f95abbec92242
John Cipriano <johnmikecip@gmail.com>
2021-02-26 21:19:40 -0800

    switch FAQ module to JS strategy

---
+++ scripts/faq.js
@@ -0,0 +1,13 @@
+function toggleFaq(event) {
+    const cls = 'faq-is-open'
+    const faq = event.target.closest('.faq')
+    const open = faq.classList.contains(cls)
+
+    faq.classList[open ? 'remove' : 'add'](cls)
+    event.preventDefault()
+}
+
+const faqs = document.querySelectorAll('.faq')
+for (const faq of faqs) {
+    faq.addEventListener('click', toggleFaq)
+}
