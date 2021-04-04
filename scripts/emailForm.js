33e1f50ad80761a2ed32558fdb557e672533ce14
John Cipriano <johnmikecip@gmail.com>
2021-04-03 22:40:28 -0700

    add JS for setting email form redirect URL

--- scripts/emailForm.js
+++ scripts/emailForm.js
@@ -1,6 +1,12 @@
-const getUpdatesLinks = document.querySelectorAll('.getUpdates')
-const emailInput = document.querySelector('.emailForm--inputBox')
+const emailForm = document.querySelector('.emailForm')
 
+/* send "get updates" links to email form */
+const emailInput = emailForm.querySelector('.emailForm--inputBox')
+const getUpdatesLinks = document.querySelectorAll('.getUpdates')
 for (const link of getUpdatesLinks) {
     link.addEventListener('click', () => emailInput.focus())
 }
+
+/* update email form redirect URL with browser's exact location */
+const emailRedirect = emailForm.querySelector('input[name="slap_redirect"]')
+emailRedirect.value = location.href
