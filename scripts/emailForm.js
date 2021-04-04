3c0b8dfd5028526fe17c5f7654b7f79db829393f
John Cipriano <johnmikecip@gmail.com>
2021-04-03 22:59:01 -0700

    get rid of custom redirect stuff

--- scripts/emailForm.js
+++ scripts/emailForm.js
@@ -6,7 +6,3 @@ const getUpdatesLinks = document.querySelectorAll('.getUpdates')
 for (const link of getUpdatesLinks) {
     link.addEventListener('click', () => emailInput.focus())
 }
-
-/* update email form redirect URL with browser's exact location */
-const emailRedirect = emailForm.querySelector('input[name="slap_redirect"]')
-emailRedirect.value = location.href
